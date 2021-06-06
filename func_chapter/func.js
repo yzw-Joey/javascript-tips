9f05e053081aae161262f411a8a2e29ce06f3a78
Groot yang <yangzhiwen5500@qq.com>
2021-06-06 16:14:13 +0800

    func chapter

--- func_chapter/func.js
+++ func_chapter/func.js
@@ -1,6 +1,25 @@
 /*
  * @Author: mikey.zhaopeng
  * @Date: 2021-06-05 21:14:13
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2021-06-05 21:15:13
+ * @Last Modified by: joey yang
+ * @Last Modified time: 2021-06-06 15:48:50
  */
+
+// bind test!
+Function.prototype.myBind = function () {
+  var args = [...arguments],
+    fn = this;
+  context = args.shift();
+  return function () {
+    return fn.apply(context, args.concat([...arguments]));
+  };
+};
+var name = "window_name";
+var obj = {
+  name: "obj_name",
+};
+var fn = function (aaa) {
+  console.log(this.name, aaa);
+};
+fn = fn.myBind(obj);
+fn("xixixi");
